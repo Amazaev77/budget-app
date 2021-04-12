bd800c11a9eb36e447204f80587531b3b6869561
Askhab Amazaev <askhabamazaev@gmail.com>
2021-04-12 11:55:44 +0300

    Update configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,5 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
 import expenses from "./features/expenses";
-import thunk from "redux-thunk";
 import categories from "./features/categories";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./features/sagas/index";
